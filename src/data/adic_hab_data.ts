7cd37d790bee2f3e751aa8e79bea83d8f05a5d3b
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2023-01-17 23:38:42 -0300

    feat: :sparkles: Create JSON database for API
    Create JSON database for API

---
+++ src/data/adic_hab_data.ts
@@ -0,0 +1,63 @@
+export const adic_hab_data: AdicHabDataProps[] = [
+    {
+        'altos_estudos_I': 30,
+        'altos_estudos_II': 25,
+        'aperfeicoamento': 20,
+        'especializacao': 16,
+        'formacao': 12,
+        'sem_formacao': 0,
+        'periodo_ini': '2001-08-31',
+        'periodo_fim': '2020-06-30'
+    },
+    {
+        'altos_estudos_I': 42,
+        'altos_estudos_II': 37,
+        'aperfeicoamento': 27,
+        'especializacao': 19,
+        'formacao': 12,
+        'sem_formacao': 0,
+        'periodo_ini': '2020-07-01',
+        'periodo_fim': '2021-06-30'
+    },
+    {
+        'altos_estudos_I': 54,
+        'altos_estudos_II': 49,
+        'aperfeicoamento': 34,
+        'especializacao': 22,
+        'formacao': 12,
+        'sem_formacao': 0,
+        'periodo_ini': '2021-07-01',
+        'periodo_fim': '2022-06-30'
+    },
+    {
+        'altos_estudos_I': 66,
+        'altos_estudos_II': 61,
+        'aperfeicoamento': 41,
+        'especializacao': 25,
+        'formacao': 12,
+        'sem_formacao': 0,
+        'periodo_ini': '2022-07-01',
+        'periodo_fim': '2023-06-30'
+    },
+    {
+        'altos_estudos_I': 73,
+        'altos_estudos_II': 68,
+        'aperfeicoamento': 45,
+        'especializacao': 27,
+        'formacao': 12,
+        'sem_formacao': 0,
+        'periodo_ini': '2023-07-01',
+        'periodo_fim': '2200-12-31'
+    },
+]
+
+export interface AdicHabDataProps {
+    altos_estudos_I: number;
+    altos_estudos_II: number;
+    aperfeicoamento: number;
+    especializacao: number;
+    formacao: number;
+    sem_formacao: number;
+    periodo_ini: string;
+    periodo_fim: string;
+}
---
+++ src/data/pg_data.ts
@@ -0,0 +1,150 @@
+export const pg_data = [
+    {
+        'startAt': '2020-01-01',
+        'endAt': '2200-12-31',
+        'Gen Ex Inat': {
+            'pg_full': 'GEN EX - INATIVIDADE', 'adic_mil': 28, 'soldo': 14031, 'adic_disp': 41, 'tipo': 'normal'
+        },
+
+        'Gen Ex': {
+            'pg_full': 'GENERAL DE EXERCITO', 'adic_mil': 28, 'soldo': 13471, 'adic_disp': 41, 'tipo': 'normal'
+        },
+
+        'Gen Div': {
+            'pg_full': 'GENERAL DE DIVISAO', 'adic_mil': 28, 'soldo': 12912, 'adic_disp': 38, 'tipo': 'normal'
+        },
+
+        'Gen Bda': {
+            'pg_full': 'GENERAL DE BRIGADA', 'adic_mil': 28, 'soldo': 12490, 'adic_disp': 35, 'tipo': 'normal'
+        },
+
+        'Cel': {
+            'pg_full': 'CORONEL', 'adic_mil': 25, 'soldo': 11451, 'adic_disp': 32, 'tipo': 'normal'
+        },
+
+        'TC': {
+            'pg_full': 'TENENTE-CORONEL', 'adic_mil': 25, 'soldo': 11250, 'adic_disp': 26, 'tipo': 'normal'
+        },
+
+        'Maj': {
+            'pg_full': 'MAJOR', 'adic_mil': 25, 'soldo': 11088, 'adic_disp': 20, 'tipo': 'normal'
+        },
+
+        'Cap': {
+            'pg_full': 'CAPITÃO', 'adic_mil': 22, 'soldo': 9135, 'adic_disp': 12, 'tipo': 'normal'
+        },
+
+        'Cap QAO': {
+            'pg_full': 'CAPITÃO QAO', 'adic_mil': 22, 'soldo': 9135, 'adic_disp': 32, 'tipo': 'especial'
+        },
+
+        '1º Ten': {
+            'pg_full': 'PRIMEIRO-TENENTE', 'adic_mil': 19, 'soldo': 8245, 'adic_disp': 6, 'tipo': 'normal'
+        },
+
+        '1º Ten QAO': {
+            'pg_full': 'PRIMEIRO-TENENTE QAO', 'adic_mil': 19, 'soldo': 8245, 'adic_disp': 32, 'tipo': 'especial'
+        },
+
+        '2º Ten': {
+            'pg_full': 'SEGUNDO-TENENTE', 'adic_mil': 19, 'soldo': 7490, 'adic_disp': 5, 'tipo': 'normal'
+        },
+
+        '2º Ten QAO': {
+            'pg_full': 'SEGUNDO-TENENTE QAO', 'adic_mil': 19, 'soldo': 7490, 'adic_disp': 32, 'tipo': 'especial'
+        },
+
+        'Asp Of': {
+            'pg_full': 'ASPIRANTE-A-OFICIAL', 'adic_mil': 19, 'soldo': 7315, 'adic_disp': 5, 'tipo': 'normal'
+        },
+
+        'Cad último ano': {
+            'pg_full': 'CADETE ULTIMO ANO', 'adic_mil': 0, 'soldo': 1630, 'adic_disp': 5, 'tipo': 'normal'
+        },
+
+        'Cad 1/2/3 ano': {
+            'pg_full': 'CADETE 1/2/3 ANO', 'adic_mil': 0, 'soldo': 1334, 'adic_disp': 5, 'tipo': 'normal'
+        },
+
+        'Al CPOR': {
+            'pg_full': 'ALUNO CPOR/NPOR/IME', 'adic_mil': 0, 'soldo': 1334, 'adic_disp': 5, 'tipo': 'normal'
+        },
+
+        'Al ES F S': {
+            'pg_full': 'ALUNO ES F S', 'adic_mil': 0, 'soldo': 1199, 'adic_disp': 5, 'tipo': 'normal'
+        },
+
+        'Al EPC': {
+            'pg_full': 'ALUNO EPC', 'adic_mil': 0, 'soldo': 1199, 'adic_disp': 5, 'tipo': 'normal'
+        },
+
+        'Al EPC 1/2/3': {
+            'pg_full': 'ALUNO EPC 1/2/3', 'adic_mil': 0, 'soldo': 1185, 'adic_disp': 5, 'tipo': 'normal'
+        },
+
+        'S Ten': {
+            'pg_full': 'SUB-TENENTE', 'adic_mil': 16, 'soldo': 6169, 'adic_disp': 32, 'tipo': 'normal'
+        },
+
+        '1º Sgt': {
+            'pg_full': 'PRIMEIRO-SARGENTO', 'adic_mil': 16, 'soldo': 5483, 'adic_disp': 20, 'tipo': 'normal'
+        },
+
+        '2º Sgt': {
+            'pg_full': 'SEGUNDO-SARGENTO', 'adic_mil': 16, 'soldo': 4770, 'adic_disp': 12, 'tipo': 'normal'
+        },
+
+        '2º Sgt QE': {
+            'pg_full': 'SEGUNDO-SARGENTO QE', 'adic_mil': 16, 'soldo': 4770, 'adic_disp': 26, 'tipo': 'especial'
+        },
+
+        '3º Sgt': {
+            'pg_full': 'TERCEIRO-SARGENTO', 'adic_mil': 16, 'soldo': 3825, 'adic_disp': 6, 'tipo': 'normal'
+        },
+
+        '3º Sgt QE': {
+            'pg_full': 'TERCEIRO-SARGENTO QE', 'adic_mil': 16, 'soldo': 3825, 'adic_disp': 16, 'tipo': 'especial'
+        },
+
+        'Cb Eng': {
+            'pg_full': 'CABO ENGAJADO', 'adic_mil': 13, 'soldo': 2627, 'adic_disp': 6, 'tipo': 'normal'
+        },
+
+        'Cb N Eng': {
+            'pg_full': 'CABO NAO ENGAJADO', 'adic_mil': 0, 'soldo': 1078, 'adic_disp': 6, 'tipo': 'normal'
+        },
+
+        'Sd Esp': {
+            'pg_full': 'SOLDADO PQDT (ENG)', 'adic_mil': 13, 'soldo': 1926, 'adic_disp': 5, 'tipo': 'normal'
+        },
+
+        'Sd 2A Cl': {
+            'pg_full': 'SD 2A CL (CLAR/CORN)', 'adic_mil': 13, 'soldo': 1765, 'adic_disp': 5, 'tipo': 'normal'
+        },
+
+        'Sd 3A Cl': {
+            'pg_full': 'SD 3A CL (CLAR/CORN)', 'adic_mil': 0, 'soldo': 1078, 'adic_disp': 5, 'tipo': 'normal'
+        },
+
+        'Sd Eng': {
+            'pg_full': 'SOLDADO DO EXERCITO', 'adic_mil': 13, 'soldo': 1765, 'adic_disp': 5, 'tipo': 'normal'
+        },
+
+        'Sd Rcr': {
+            'pg_full': 'SOLDADO RECRUTA', 'adic_mil': 0, 'soldo': 1078, 'adic_disp': 5, 'tipo': 'normal'
+        },
+
+        'TF Mor': {
+            'pg_full': 'TAIFEIRO-MOR', 'adic_mil': 13, 'soldo': 2627, 'adic_disp': 5, 'tipo': 'normal'
+        },
+
+        'TF 1Cl': {
+            'pg_full': 'TAIFEIRO 1 CL', 'adic_mil': 13, 'soldo': 2325, 'adic_disp': 5, 'tipo': 'normal'
+        },
+
+        'TF 2Cl': {
+            'pg_full': 'TAIFEIRO 2 CL', 'adic_mil': 13, 'soldo': 2210, 'adic_disp': 5, 'tipo': 'normal'
+        },
+    }
+
+]
