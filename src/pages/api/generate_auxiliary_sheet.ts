817c326aaf33ccc0e61a4f3302bffbb35c972358
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2023-01-21 16:01:30 -0300

    refactor: :recycle: Change response format
    Change response format

--- src/pages/api/generate_auxiliary_sheet.ts
+++ src/pages/api/generate_auxiliary_sheet.ts
@@ -5,50 +5,58 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     const data = req.body
     const auxiliarySheet = new AuxiliarySheetEtitie(data)
 
+    const receitasList = [
+        auxiliarySheet.soldo,
+        auxiliarySheet.complementoCotaSoldo,
+        auxiliarySheet.adicTpSv,
+        auxiliarySheet.adicCompDispMil,
+        auxiliarySheet.adicHab,
+        auxiliarySheet.adicMil,
+        auxiliarySheet.adicPerm,
+        auxiliarySheet.adicCoOrg,
+        auxiliarySheet.adicHVoo,
+        auxiliarySheet.acres25Soldo,
+        auxiliarySheet.adicPTTC,
+        auxiliarySheet.adicFerias,
+        auxiliarySheet.adicNatalino,
+        auxiliarySheet.salarioFamilia,
+        auxiliarySheet.auxTransporte,
+        auxiliarySheet.auxNatalidade,
+        auxiliarySheet.auxInvalidez,
+        ...auxiliarySheet.auxPreEsc,
+        auxiliarySheet.auxFardamento,
+        auxiliarySheet.auxAlimentC,
+        auxiliarySheet.auxAliment,
+        auxiliarySheet.gratLocEsp,
+        auxiliarySheet.gratRepCmdo,
+    ].filter(item => item.value > 0)
+
+    const descontosList = [
+        ...auxiliarySheet.pMil,
+        auxiliarySheet.pMilExt,
+        auxiliarySheet.fusex,
+        auxiliarySheet.descDepFusex,
+        ...auxiliarySheet.pnr,
+        ...auxiliarySheet.pensaoJudiciaria,
+        ...auxiliarySheet.pensaoJudiciariaAdicNatal,
+        auxiliarySheet.dedAdAdicNatalino,
+        auxiliarySheet.irMensal,
+        auxiliarySheet.irFerias,
+        auxiliarySheet.irAdicNatalino,
+    ].filter(item => item.value > 0)
+
     res.status(200).json({
         receitas: [
-            auxiliarySheet.soldo,
-            auxiliarySheet.complementoCotaSoldo,
-            auxiliarySheet.adicTpSv,
-            auxiliarySheet.adicCompDispMil,
-            auxiliarySheet.adicHab,
-            auxiliarySheet.adicMil,
-            auxiliarySheet.adicPerm,
-            auxiliarySheet.adicCoOrg,
-            auxiliarySheet.adicHVoo,
-            auxiliarySheet.acres25Soldo,
-            auxiliarySheet.adicPTTC,
-            auxiliarySheet.adicFerias,
-            auxiliarySheet.adicNatalino,
-            auxiliarySheet.salarioFamilia,
-            auxiliarySheet.auxTransporte,
-            auxiliarySheet.auxNatalidade,
-            auxiliarySheet.auxInvalidez,
-            ...auxiliarySheet.auxPreEsc,
-            auxiliarySheet.auxFardamento,
-            auxiliarySheet.auxAlimentC,
-            auxiliarySheet.auxAliment,
-            auxiliarySheet.gratLocEsp,
-            auxiliarySheet.gratRepCmdo,
+            ...receitasList,
             ...auxiliarySheet.extraValues.receitas,
         ],
         descontos: [
-            ...auxiliarySheet.pMil,
-            auxiliarySheet.pMilExt,
-            auxiliarySheet.fusex,
-            auxiliarySheet.descDepFusex,
-            ...auxiliarySheet.pnr,
-            ...auxiliarySheet.pensaoJudiciaria,
-            ...auxiliarySheet.pensaoJudiciariaAdicNatal,
-            auxiliarySheet.dedAdAdicNatalino,
-            auxiliarySheet.irMensal,
-            auxiliarySheet.irFerias,
-            auxiliarySheet.irAdicNatalino,
+            ...descontosList,
             ...auxiliarySheet.extraValues.descontos,
         ],
-
-        data: [
-            data
-        ]
+        somatorios: {
+            receitas: receitasList.reduce((prev, item) => prev + item.value, 0) + auxiliarySheet.extraValues.receitas.reduce((prev, item) => prev + item.value, 0),
+            descontos: descontosList.reduce((prev, item) => prev + item.value, 0) + auxiliarySheet.extraValues.descontos.reduce((prev, item) => prev + item.value, 0),
+        }
     })
 }
