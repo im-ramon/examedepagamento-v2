c70b8e4ae6d85c1dc5aab352fc13e0d699d3858b
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2023-02-01 21:39:14 -0300

    feat: :sparkles: Add NODE_ENV to change between dev and prod mod
    Add NODE_ENV to change between dev and prod mod

--- src/services/pocktbase_api.ts
+++ src/services/pocktbase_api.ts
@@ -1,15 +1,8 @@
 import axios from "axios";
 
-// Install dot.env after to fix it
-// Development:
 const pocktbase_api = axios.create({
-    baseURL: 'http://127.0.0.1:8090/api',
+    baseURL: process.env.NODE_ENV == 'production' ? 'http://app/api' : 'http://127.0.0.1:8090/api',
 })
-
-// Production:
-// const api = axios.create({
-//     baseURL: 'http://app/api',
-// })
 
 
 export { pocktbase_api };
