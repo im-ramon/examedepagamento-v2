6277a43d59aa0ff3cc71a21bf4cfe87ca1c7022a
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2023-03-06 22:01:40 -0300

    fix: :bug: Fix API
    Fix API

--- src/services/pocktbase.ts
+++ src/services/pocktbase.ts
@@ -2,10 +2,10 @@ import PocketBase from 'pocketbase';
 
 // Install dot.env after to fix it
 // Development:
-const pb = new PocketBase('http://127.0.0.1:8090');
+// const pb = new PocketBase('http://127.0.0.1:8090');
 
 // Production:
-//  const pb = new PocketBase('http://app');
+const pb = new PocketBase('pocktbase');
 
 export { pb };
 
--- src/services/pocktbase_api.ts
+++ src/services/pocktbase_api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const pocktbase_api = axios.create({
-    baseURL: process.env.NODE_ENV == 'production' ? 'http://app/api' : 'http://127.0.0.1:8090/api',
+    baseURL: 'pocktbase',
 })
 
 
